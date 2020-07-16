a49ff0b2902b737be04ef82969df42fd45440375
nohyoon <kny_1011@naver.com>
2020-07-16 09:05:13 +0900

    creating web handler by using decorator pattern

---
+++ 6-WebDecoratorHandler/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"6-WebDecoratorHandler/myapp"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/tuckersGo/goWeb/web10/decoHandler"
+)
+
+func logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Print("[LOGGER1] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER1] Completed time: ", time.Since(start).Milliseconds())
+}
+
+func logger2(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Print("[LOGGER2] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER2] Completed time: ", time.Since(start).Milliseconds())
+}
+
+func NewHandler() http.Handler {
+	h := myapp.NewHandler()
+	h = decoHandler.NewDecoHandler(h, logger)
+	h = decoHandler.NewDecoHandler(h, logger2)
+	return h
+}
+
+func main() {
+	mux := NewHandler()
+	http.ListenAndServe(":3000", mux)
+}
